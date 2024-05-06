9faa308dd451cda1caaa50fb78a253690511a3f8
kwiruu <keiru277@gmail.com>
2024-05-06 23:04:12 +0800

    pushed files! image implementation!

--- core/src/main/java/com/libgdx/undercooked/Main.java
+++ core/src/main/java/com/libgdx/undercooked/Main.java
@@ -7,7 +7,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -28,6 +30,7 @@ public class Main extends ApplicationAdapter {
     private OrthogonalTiledMapRenderer tmr;
     private TiledMap map;
     private Box2DDebugRenderer b2dr;
+    private Texture backgroundTexture1,backgroundTexture2,backgroundTexture3,backgroundTexture4,backgroundTexture5;
 
     private World world;
     private Body player;
@@ -46,7 +49,7 @@ public class Main extends ApplicationAdapter {
         world = new World(new Vector2(0f,0f), false);
         b2dr = new Box2DDebugRenderer();
 
-        player = createBox(8,2,32,32,false);
+        player = createBox(8,2,16,8,false);
         Body platform = createBox(8, 0, 64, 32, true);
 
         batch = new SpriteBatch();
@@ -55,25 +58,48 @@ public class Main extends ApplicationAdapter {
         map = new TmxMapLoader().load("assets/maps/test_map.tmx");
         tmr = new OrthogonalTiledMapRenderer(map);
 
+        // ako nalang ge image kay impossible ang 2k nga sriptes lmao
+        backgroundTexture1 = new Texture("assets/maps/test_map/test_map_blackwall.png");
+        backgroundTexture2 = new Texture("assets/maps/test_map/test_map_wall.png");
+        backgroundTexture3 = new Texture("assets/maps/test_map/test_map_behind_player.png");
+        backgroundTexture4 = new Texture("assets/maps/test_map/test_map_furnitures.png");
+        backgroundTexture5 = new Texture("assets/maps/test_map/test_map_on-top.png");
+
         TiledObjectUtil.parseTiledObjectLayer(world,map.getLayers().get("collision_layer").getObjects());
     }
 
     @Override
     public void render() {
-
         update(Gdx.graphics.getDeltaTime());
-        Gdx.gl.glClearColor(1f,1f,1f,1f);
+        Gdx.gl.glClearColor(58 / 255f, 58 / 255f, 80 / 255f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        batch.draw(texture,player.getPosition().x * PPM - (texture.getWidth()/2),player.getPosition().y * PPM - (texture.getHeight()/4));
+
+        // manually add the images by layer!
+        batch.draw(backgroundTexture1, 0, 0);
+        batch.draw(backgroundTexture2, 0, 0);
+        batch.draw(texture, player.getPosition().x * PPM - (texture.getWidth() / 2), player.getPosition().y * PPM - (texture.getHeight() / 8));
+        batch.draw(backgroundTexture3, 0, 0);
         batch.end();
 
-        tmr.render();
+        for (int i = 0; i < map.getLayers().getCount(); i++) {
+            if (shouldRenderLayer(i)) {
+                tmr.renderTileLayer((TiledMapTileLayer) map.getLayers().get(i));
+            }
+        }
 
         b2dr.render(world, camera.combined.scl(PPM));
 
-        if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
+    }
+
+    private boolean shouldRenderLayer(int layerIndex) {
+        // Add your logic here to determine which layers to render
+        // For example, you can check the layer names or indices
+        // Return true for layers you want to render, false for others
+        String layerName = map.getLayers().get(layerIndex).getName();
+        return layerName.equals("background_layer") || layerName.equals("object_layer");
     }
 
     private void update(float deltaTime) {
@@ -86,8 +112,9 @@ public class Main extends ApplicationAdapter {
     }
 
     public void inputUpdate(float deltaTime){
-        int horizontalForce = 0;
-        int verticalForce = 0;
+
+        float horizontalForce = 0;
+        float verticalForce = 0;
 
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
             verticalForce += 1;
@@ -126,8 +153,15 @@ public class Main extends ApplicationAdapter {
         b2dr.dispose();
         batch.dispose();
         texture.dispose();
+        backgroundTexture1.dispose();
+        backgroundTexture2.dispose();
+        backgroundTexture3.dispose();
+        backgroundTexture4.dispose();
+        backgroundTexture5.dispose();
         tmr.dispose();
         map.dispose();
+
+
     }
 
     public Body createBox(int x, int y, int width, int height, boolean isStatic){
--- lwjgl3/src/main/java/com/libgdx/undercooked/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/libgdx/undercooked/lwjgl3/Lwjgl3Launcher.java
@@ -24,7 +24,7 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(1366, 768);
+
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
