4b53b3d08426abd7dda8c976fc0999b6e58ccded
kwiruu <keiru277@gmail.com>
2024-05-06 20:36:23 +0800

    pushed files! continue on part 3! LibGDX - Box2D/TiledMap - Creating our TiledMap (Part 1)

---
+++ assets/maps/Interiors_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Interiors_32x32" tilewidth="32" tileheight="32" tilecount="17024" columns="16">
+ <image source="../tilesets/Interiors_32x32.png" width="512" height="34048"/>
+</tileset>
---
+++ assets/maps/Room_Builder_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Room_Builder_32x32" tilewidth="32" tileheight="32" tilecount="8284" columns="76">
+ <image source="../tilesets/Room_Builder_32x32.png" width="2432" height="3488"/>
+</tileset>
