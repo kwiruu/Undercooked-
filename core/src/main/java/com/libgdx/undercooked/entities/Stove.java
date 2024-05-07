ef0fe9bc4facdda39f3127e858bfe60522618631
LAB14_USER ON WS41 <L14Y15W41@citulab.net>
2024-05-07 17:53:55 +0800

    added entities - alec

--- core/src/main/java/com/libgdx/undercooked/entities/Stove.java
+++ core/src/main/java/com/libgdx/undercooked/entities/Stove.java
@@ -3,8 +3,8 @@ package com.libgdx.undercooked.entities;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 
 public class Stove extends Station {
-    public Stove(float x, float y, int width, int height, boolean isStatic) {
-        super(x, y, width, height, isStatic);
+    public Stove(float x, float y, int width, int height) {
+        super(x, y, width, height);
     }
     // TODO station to continue
 }
