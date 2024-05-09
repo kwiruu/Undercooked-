b7f130d3b3603941c7711b0e22395de02dcbd2e1
kwiruu <keiru277@gmail.com>
2024-05-09 17:20:07 +0800

    changed some sprites

--- assets/objects.tsx
+++ assets/objects.tsx
@@ -2,9 +2,6 @@
 <tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="32" tileheight="64" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="stove">
-  <properties>
-   <property name="Name" value="stove"/>
-  </properties>
   <image width="32" height="64" source="stations/stove-on.png"/>
  </tile>
 </tileset>
