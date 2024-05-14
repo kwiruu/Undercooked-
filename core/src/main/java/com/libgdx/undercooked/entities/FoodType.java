4c38900a315b2fe580b016dfe2c7b86d32a2a441
soal-ec <165453551+soal-ec@users.noreply.github.com>
2024-05-14 17:25:11 +0800

    added more FoodTypes

--- core/src/main/java/com/libgdx/undercooked/entities/FoodType.java
+++ core/src/main/java/com/libgdx/undercooked/entities/FoodType.java
@@ -5,8 +5,8 @@ public enum FoodType {
 
     tomato, chopped_tomato, tomato_soup,
     onion, chopped_onion,
-    meat, chopped_meat,
-    fish, chopped_fish,
-    pickle, chopped_pickle;
-
+    meat, chopped_meat, cooked_meat,
+    fish, chopped_fish, cooked_fish,
+    pickle, chopped_pickle,
+    rice, meat_meal, fish_meal
 }
